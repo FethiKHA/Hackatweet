13e5841ce2703a3ba5b43ee876ec49850a661f16
Fethi <khalifethi@live.fr>
2024-11-21 12:27:15 +0100

    maou

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const connectionString = 'mongodb+srv://khalifethi:Tesveuch@cluster0.emx75.mongodb.net/Hackatweet';
+
+const connectionString = process.env.MangoURL
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
  .then(() => console.log('Database connected'))
